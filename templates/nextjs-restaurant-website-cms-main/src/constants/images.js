53a33ebe48dff0c1f1f27c7d05b7dd9efd63368d
vhthien611 <thien.vh611@gmail.com>
2024-02-21 12:05:04 +0700

    update

--- templates/nextjs-restaurant-website-cms-main/src/constants/images.js
+++
@@ -1,7 +0,0 @@
-import spoon from '../assets/spoon.svg';
-import sign from '../assets/sign.png';
-
-export default {
-  spoon,
-  sign,
-};
--- templates/nextjs-restaurant-website-cms-main/src/utils/chooseValueByType.js
+++
@@ -1,8 +0,0 @@
-const chooseByType = (data, slugName) => {
-  if( data && slugName ) {
-    const chooseBySlug = data?.filter(content => Object.values(content).includes(slugName));
-    return chooseBySlug ? chooseBySlug[0] : [];
-  }
-}
-
-export default chooseByType;
